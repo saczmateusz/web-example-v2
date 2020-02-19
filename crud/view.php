a0474250b4210e70142f3ee47190dee95c9007b2
Mateusz Sączawa <sacz.mateusz@gmail.com>
2020-02-19 14:47:04 +0100

    crud-view style

--- crud/view.php
+++ crud/view.php
@@ -22,8 +22,6 @@ if (!$city) {
   <title>Pogoda</title>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="shortcut icon" type="image/png" href="../assets/img/favicon.png" />
   <link rel="stylesheet" href="../assets/css/styles.css" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" />
@@ -34,24 +32,19 @@ if (!$city) {
     <div class="header-title">
       <h1 class="animate-left">Panel administratora</h1>
     </div>
-    <div class="header-form animate-right">
-      <form class="searchbar" id="cityform" method="POST">
-        <button name="logout" class="button red-button">Wyloguj</button>
-      </form>
-    </div>
   </header>
 
   <div class="content container">
     <div class="card">
       <div class="card-header">
         <h3>Zobacz miasto: <b><?php echo $city['name'] ?></b></h3>
-      </div>
-      <div class="card-body">
-        <a class="btn btn-secondary" href="update.php?id=<?php echo $city['id'] ?>">Edytuj</a>
-        <form style="display: inline-block" method="POST" action="delete.php">
-          <input type="hidden" name="id" value="<?php echo $city['id'] ?>">
-          <button class="btn btn-danger">Usuń</button>
-        </form>
+        <div class="view-buttons">
+          <a class="button edit" href="update.php?id=<?php echo $city['id'] ?>">Edytuj</a>
+          <form style="display: inline-block" method="POST" action="delete.php">
+            <input type="hidden" name="id" value="<?php echo $city['id'] ?>">
+            <button class="button delete">Usuń</button>
+          </form>
+        </div>
       </div>
       <table class="table">
         <tbody>
@@ -84,6 +77,9 @@ if (!$city) {
           </tr>
         </tbody>
       </table>
+      <div class="back">
+        <a href="../admin/admin-dashboard.php" class="button view">Wróć</a>
+      </div>
     </div>
   </div>
 
