c37ed34d55ff9f6f77f301c9fc4aa933c743616f
Mateusz Sączawa <sacz.mateusz@gmail.com>
2020-02-19 02:19:17 +0100

    admin-login and crud-view temporary style

--- admin/admin-login.php
+++ admin/admin-login.php
@@ -21,64 +21,61 @@ if (isset($_POST['submit'])) {
 
 ?>
 <html>
+
 <head>
-    <title>Admin Login</title>
-    <link rel="shortcut icon" type="image/png" href="../assets/img/favicon.png" />
-    <link rel="stylesheet" href="../assets/css/styles.css" />
+  <title>Admin Login</title>
+  <link rel="shortcut icon" type="image/png" href="../assets/img/favicon.png" />
+  <link rel="stylesheet" href="../assets/css/styles.css" />
 </head>
+
 <body>
-    <header>
-      <div class="header-title">
-        <h1 class="animate-left">Admin Login</h1>
-      </div>
-      <div class="header-form animate-right">
-        <form class="searchbar" id="cityform">
-          <input
-            class="form-input"
-            type="text"
-            id="city"
-            placeholder="Wyszukaj miasto"
-          />
-          <button class="form-submit button" type="submit">
-            DODAJ
-          </button>
-        </form>
-      </div>
-    </header>
-
-    <div class="content" id="panels">
-      <div class="panel" id="template">
-        <form method="POST">
-          <div class="login-form">
-            <label for="login">Login</label>
-            <input type="text" name="login">
-            <label for="password">Hasło</label>
-            <input type="password" name="password">
-            <button name="submit">Zaloguj</button>
-            <?php if ($msg): ?>
-            <p> <?php echo $msg; ?> </p>
-            <?php endif;?>
-          </div>
-        </form>
-      </div>
+  <header>
+    <div class="header-title">
+      <h1 class="animate-left">Admin Login</h1>
     </div>
-
-    <footer>
-      <p>
-        © 2020 Mateusz Sączawa. All rights reserved.
-      </p>
-      <div class="clear">
-        <button class="button" onclick="clearLocalStorage()">
-          WYCZYŚĆ
+    <div class="header-form animate-right">
+      <form class="searchbar" id="cityform">
+        <input class="form-input" type="text" id="city" placeholder="Wyszukaj miasto" />
+        <button class="form-submit button" type="submit">
+          DODAJ
         </button>
-      </div>
-    </footer>
-
-    <div class="loading">
-      <img src="../assets/img/planet.svg" />
+      </form>
     </div>
+  </header>
 
-    <script src="../assets/js/script.js"></script>
-  </body>
+  <div class="content" id="panels">
+    <div class="panel" id="template">
+      <form method="POST">
+        <div class="login-form">
+          <label for="login">Login</label>
+          <input type="text" name="login">
+          <label for="password">Hasło</label>
+          <input type="password" name="password">
+          <button name="submit">Zaloguj</button>
+          <?php if ($msg): ?>
+          <p> <?php echo $msg; ?> </p>
+          <?php endif;?>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <footer>
+    <p>
+      © 2020 Mateusz Sączawa. All rights reserved.
+    </p>
+    <div class="clear">
+      <button class="button red-button" onclick="clearLocalStorage()">
+        WYCZYŚĆ
+      </button>
+    </div>
+  </footer>
+
+  <div class="loading">
+    <img src="../assets/img/planet.svg" />
+  </div>
+
+  <script src="../assets/js/script.js"></script>
+</body>
 
 </html>
--- crud/view.php
+++ crud/view.php
@@ -1,5 +1,4 @@
 <?php
-include 'components/header.php';
 require '../cities/cities.php';
 
 if (!isset($_GET['id'])) {
@@ -15,49 +14,85 @@ if (!$city) {
 }
 
 ?>
-<div class="container">
-    <div class="card">
-        <div class="card-header">
-            <h3>Zobacz miasto: <b><?php echo $city['name'] ?></b></h3>
-        </div>
-        <div class="card-body">
-            <a class="btn btn-secondary" href="update.php?id=<?php echo $city['id'] ?>">Edytuj</a>
-            <form style="display: inline-block" method="POST" action="delete.php">
-                <input type="hidden" name="id" value="<?php echo $city['id'] ?>">
-                <button class="btn btn-danger">Usuń</button>
-            </form>
-        </div>
-        <table class="table">
-            <tbody>
-            <tr>
-                <th>Nazwa:</th>
-                <td><?php echo $city['name'] ?></td>
-            </tr>
-            <tr>
-                <th>Temperatura:</th>
-                <td><?php echo $city['temperature'] ?></td>
-            </tr>
-            <tr>
-                <th>Wilgotność:</th>
-                <td><?php echo $city['humidity'] ?></td>
-            </tr>
-            <tr>
-                <th>Wiatr:</th>
-                <td><?php echo $city['wind'] ?></td>
-            </tr>
-            <tr>
-                <th>Opis:</th>
-                <td><?php echo $city['description'] ?></td>
-            </tr>
-            <tr>
-                <th>Ikona:</th>
-                <td>
-                <img style="width: 60px" src="<?php echo "http://openweathermap.org/img/wn/" . $city['icon'] . "@2x.png" ?>" alt="pogoda">
-                </td>
-            </tr>
-            </tbody>
-        </table>
+
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+  <title>Pogoda</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="shortcut icon" type="image/png" href="../assets/img/favicon.png" />
+  <link rel="stylesheet" href="../assets/css/styles.css" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" />
+</head>
+
+<body>
+  <header>
+    <div class="header-title">
+      <h1 class="animate-left">Panel administratora</h1>
     </div>
-</div>
+    <div class="header-form animate-right">
+      <form class="searchbar" id="cityform" method="POST">
+        <button name="logout" class="button red-button">Wyloguj</button>
+      </form>
+    </div>
+  </header>
+
+  <div class="content container">
+    <div class="card">
+      <div class="card-header">
+        <h3>Zobacz miasto: <b><?php echo $city['name'] ?></b></h3>
+      </div>
+      <div class="card-body">
+        <a class="btn btn-secondary" href="update.php?id=<?php echo $city['id'] ?>">Edytuj</a>
+        <form style="display: inline-block" method="POST" action="delete.php">
+          <input type="hidden" name="id" value="<?php echo $city['id'] ?>">
+          <button class="btn btn-danger">Usuń</button>
+        </form>
+      </div>
+      <table class="table">
+        <tbody>
+          <tr>
+            <th>Nazwa:</th>
+            <td><?php echo $city['name'] ?></td>
+          </tr>
+          <tr>
+            <th>Temperatura:</th>
+            <td><?php echo $city['temperature'] ?></td>
+          </tr>
+          <tr>
+            <th>Wilgotność:</th>
+            <td><?php echo $city['humidity'] ?></td>
+          </tr>
+          <tr>
+            <th>Wiatr:</th>
+            <td><?php echo $city['wind'] ?></td>
+          </tr>
+          <tr>
+            <th>Opis:</th>
+            <td><?php echo $city['description'] ?></td>
+          </tr>
+          <tr>
+            <th>Ikona:</th>
+            <td>
+              <img style="width: 60px"
+                src="<?php echo "http://openweathermap.org/img/wn/" . $city['icon'] . "@2x.png" ?>" alt="pogoda">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <footer>
+    <p>
+      © 2020 Mateusz Sączawa. All rights reserved.
+    </p>
+  </footer>
+
 </body>
+
 </html>
