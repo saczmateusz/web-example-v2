ef35674ddf6ffa20d7e9579354b8d67fb3fbbf9a
Mateusz Sączawa <sacz.mateusz@gmail.com>
2020-02-19 15:56:16 +0100

    admin-login changes

--- admin/admin-login.php
+++ admin/admin-login.php
@@ -19,41 +19,45 @@ if (isset($_POST['submit'])) {
 }
 
 ?>
-<html>
+
+<!DOCTYPE html>
+<html lang="pl">
 
 <head>
   <title>Admin Login</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="shortcut icon" type="image/png" href="../assets/img/favicon.png" />
   <link rel="stylesheet" href="../assets/css/styles.css" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" />
 </head>
 
 <body>
   <header>
     <div class="header-title">
-      <h1 class="animate-left">Admin Login</h1>
+      <h1 class="animate-left">Logowanie</h1>
     </div>
-    <div class="header-form animate-right">
-      <form class="searchbar" id="cityform">
-        <input class="form-input" type="text" id="city" placeholder="Wyszukaj miasto" />
-        <button class="form-submit button" type="submit">
-          DODAJ
-        </button>
-      </form>
+    <div class="animate-right">
+      <a href="../" class="button delete">Wróć</a>
     </div>
   </header>
 
-  <div class="content" id="panels">
-    <div class="panel" id="template">
-      <form method="POST">
-        <div class="login-form">
+  <div class="content container">
+    <div class="card login-card">
+      <form action="" enctype="multipart/form-data" method="POST">
+        <div class="form-group">
           <label for="login">Login</label>
-          <input type="text" name="login">
-          <label for="password">Hasło</label>
-          <input type="password" name="password">
-          <button name="submit">Zaloguj</button>
-          <?php if ($msg): ?>
-          <p> <?php echo $msg; ?> </p>
-          <?php endif;?>
+          <input type="text" name="login" class="form-control <?php echo $msg ? 'is-invalid' : ''; ?>">
+          <div class="form-group">
+            <label for="password">Hasło</label>
+            <input type="password" name="password" class="form-control <?php echo $msg ? 'is-invalid' : ''; ?>">
+            <?php if ($msg): ?>
+            <div class="invalid-feedback"><?php echo $msg; ?></div>
+            <?php endif;?>
+          </div>
+          <div class="form-group submit-login">
+            <button name="submit" class="button view">Zaloguj</button>
+          </div>
         </div>
       </form>
     </div>
@@ -63,18 +67,13 @@ if (isset($_POST['submit'])) {
     <p>
       © 2020 Mateusz Sączawa. All rights reserved.
     </p>
-    <div class="clear">
-      <button class="button red-button" onclick="clearLocalStorage()">
-        WYCZYŚĆ
-      </button>
-    </div>
   </footer>
 
   <div class="loading">
     <img src="../assets/img/planet.svg" />
   </div>
 
-  <script src="../assets/js/script.js"></script>
+  <!-- <script src="../assets/js/script.js"></script> -->
 </body>
 
 </html>
